a4c63dab49a440b4bff4376c7b379ced353da084
MTaslam2000 <taiyab.aslam728@gmail.com>
2023-07-21 14:00:35 -0500

    finished  my form

--- src/RecipeCreate.js
+++ src/RecipeCreate.js
@@ -1,17 +1,54 @@
 import React, { useState } from "react";
 
 function RecipeCreate() {
-
-  // TODO: When the form is submitted, a new recipe should be created, and the form contents cleared.
-  // TODO: Add the required input and textarea form elements.
   // TODO: Add the required submit and change handlers
-  
+
+  // TODO: Add the required input and textarea form elements.
   return (
     <form name="create">
       <table>
         <tbody>
           <tr>
-            <td></td>
+            <td>
+              <label htmlFor="name">
+                Name
+                <input name="name" type="text" id="name" />
+              </label>
+            </td>
+            <td>
+              <label htmlFor="Cuisine">
+                Cuisine
+                <input name="cuisine" type="text" id="Cuisine" />
+              </label>
+            </td>
+            <td>
+              <label htmlFor="photo">
+                URL
+                <input name="photo" type="url" id="photo" />
+              </label>
+            </td>
+            <td>
+              <label htmlFor="ingredients">
+                Ingredients
+                <textarea
+                  name="ingredients"
+                  id="ingredients"
+                  rows="2"
+                  cols="20"
+                />
+              </label>
+            </td>
+            <td>
+              <label htmlFor="preparation">
+                preparation
+                <textarea
+                  name="preparation"
+                  id="preparation"
+                  rows="2"
+                  cols="20"
+                />
+              </label>
+            </td>
             <td>
               <button type="submit">Create</button>
             </td>
